97bed3a2bf42f426c3768f9d211265ee76b26bfc
edwin-otieno <edwin.otieno@capenetglobal.com>
2025-05-22 09:52:29 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://hdnaqkziirehubztmueh.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkbmFxa3ppaXJlaHVienRtdWVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5OTUyNDcsImV4cCI6MjA2MTU3MTI0N30.9zJSsbjQc8XIyEnWIQgwopNpXPrwQqjuj0_nmDG56L4";
 
 if (!SUPABASE_URL || !SUPABASE_PUBLISHABLE_KEY) {
   throw new Error('Missing Supabase environment variables');
